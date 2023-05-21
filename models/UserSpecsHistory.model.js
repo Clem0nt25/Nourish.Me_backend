38cf4ccf123de43be1874cebbab2430ddf1a94b8
Zijun Wang <zijunwang2020@gmail.com>
2023-05-21 23:14:39 +0100

    modified userSpecs model for questionnaire input

--- models/UserSpecsHistory.model.js
+++ models/UserSpecsHistory.model.js
@@ -1,68 +1,99 @@
 const { Schema, model } = require("mongoose");
 
-const userSpecsHistorySchema = new Schema(
-    {
-        currentWeight: {
-            type: Number,
-            required: true,
-        },
-        goalWeight: {
-            type: Number,
-            required: true,
-        },
-        currentCalories: {
-            type: Number,
-            required: true,
-        },
-        goalCalories: {
-            type: Number,
-            required: true,
-        },
-        currentProtein: {
-            type: Number,
-            required: true,
-        },
-        goalProtein: {
-            type: Number,
-            required: true,
-        },
-        currentCarbs: {
-            type: Number,
-            required: true,
-        },
-        goalCarbs: {
-            type: Number,
-            required: true,
-        },
-        currentFat: {
-            type: Number,
-            required: true,
-        },
-        goalFat: {
-            type: Number,
-            required: true,
-        },
-        currentFiber: {
-            type: Number,
-            required: true,
-        },
-        goalFiber: {
-            type: Number,
-            required: true,
-        },
-        date: {
-            type: String,
-            required: true,
-            default: () => new Date().toISOString().split('T')[0]
-        },
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        }
-    }
-);
+const userSpecsHistorySchema = new Schema({
+	username: {
+		type: String,
+		default: "New Nourish User",
+	},
+	gender: {
+		type: String,
+		enum: ["female", "male"],
+		require: true,
+	},
+	yearOfBirth: {
+		type: Number,
+		require: true,
+	},
+	height: {
+		type: Number,
+		require: true,
+	},
+	mainGoal: {
+		type: String,
+		enum: ["bulk-up", "get-strong", "recompose", "get-lean", "keep-shape"],
+		require: true,
+	},
+	activityLevel: {
+		type: String,
+		enum: ["sedentary", "light", "moderate", "active", "intense"],
+		require: true,
+	},
+	currentWeight: {
+		type: Number,
+		required: true,
+	},
+	goalWeight: {
+		type: Number,
+		required: true,
+	},
+	currentCalories: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	goalCalories: {
+		type: Number,
+		required: true,
+	},
+	currentProtein: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	goalProtein: {
+		type: Number,
+		required: true,
+	},
+	currentCarbs: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	goalCarbs: {
+		type: Number,
+		required: true,
+	},
+	currentFat: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	goalFat: {
+		type: Number,
+		required: true,
+		default: 70,
+	},
+	currentFiber: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	goalFiber: {
+		type: Number,
+		required: true,
+		default: 30,
+	},
+	date: {
+		type: String,
+		required: true,
+		default: () => new Date().toISOString().split("T")[0],
+	},
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+});
 
 const UserSpecsHistory = model("UserSpecsHistory", userSpecsHistorySchema);
 module.exports = UserSpecsHistory;
-
