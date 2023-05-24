f7c0c86d32f36f6046416113ee6e199d9b5f88b6
Zijun Wang <zijunwang2020@gmail.com>
2023-05-24 16:23:08 +0100

    delete extra food export

--- models/Food.model.js
+++ models/Food.model.js
@@ -5,49 +5,46 @@
 const { Schema, model } = require("mongoose");
 
 const foodSchema = new Schema({
-  foodName: {
-    type: String,
-    required: true,
-  },
-  barcode: {
-    type: String,
-    required: true,
-  },
-  calories: {
-    type: Number,
-    required: true,
-  },
-  protein: {
-    type: Number,
-    required: true,
-  },
-  fiber: {
-    type: Number,
-    required: true,
-  },
-  carbs: {
-    type: Number,
-    required: true,
-  },
-  fat: {
-    type: Number,
-    required: true,
-  },
-  // mealId from Meal.model.js
-  mealId: {
-    type: Schema.Types.ObjectId,
-    ref: "Meal",
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  amount: Number,
-  image: String,
+	foodName: {
+		type: String,
+		required: true,
+	},
+	barcode: {
+		type: String,
+		required: true,
+	},
+	calories: {
+		type: Number,
+		required: true,
+	},
+	protein: {
+		type: Number,
+		required: true,
+	},
+	fiber: {
+		type: Number,
+		required: true,
+	},
+	carbs: {
+		type: Number,
+		required: true,
+	},
+	fat: {
+		type: Number,
+		required: true,
+	},
+	// mealId from Meal.model.js
+	mealId: {
+		type: Schema.Types.ObjectId,
+		ref: "Meal",
+	},
+	date: {
+		type: String,
+		required: true,
+	},
+	amount: Number,
+	image: String,
 });
-
-const Food = model("Food", foodSchema);
-exports.Food = Food;
 
 const Food = model("Food", foodSchema);
 exports.Food = Food;
